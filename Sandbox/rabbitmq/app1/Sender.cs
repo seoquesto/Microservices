8336fdd66c9ebe28d5f1c4e905161ce8e960f5c3
seoquesto <seoquesto@gmail.com>
2021-03-06 14:10:48 +0100

    Rabbitmq sandobox

---
+++ Sandbox/rabbitmq/app1/Sender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace app1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var factory = new ConnectionFactory() { HostName = "localhost" };
+      using (var connection = factory.CreateConnection())
+      using (var channel = connection.CreateModel())
+      {
+        channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+
+        for (int i = 0; i < 6; i++)
+        {
+          string message = $"[i:{i}]" + new String('.', i);
+          var body = Encoding.UTF8.GetBytes(message);
+          channel.BasicPublish(exchange: "",
+                             routingKey: "hello",
+                             basicProperties: null,
+                             body: body);
+
+          Console.WriteLine("Send: " + message);
+        }
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Sandbox/rabbitmq/app2/Receiver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace app2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var factory = new ConnectionFactory() { HostName = "localhost" };
+      using (var connection = factory.CreateConnection())
+      using (var channel = connection.CreateModel())
+      {
+        channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+          var body = ea.Body.ToArray();
+          var message = Encoding.UTF8.GetString(body);
+          var dots = message.Split(".").Length;
+          Console.WriteLine("Processing Received {0}", message);
+          Thread.Sleep(dots * 1000);
+          Console.WriteLine("Received Received {0}", message);
+          channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        };
+        channel.BasicConsume(queue: "hello",
+                             autoAck: false,
+                             consumer: consumer);
+
+        Console.ReadLine();
+      }
+    }
+  }
+}
